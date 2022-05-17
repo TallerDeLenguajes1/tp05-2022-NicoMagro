e0f75fd405c1800f728d03072e5badb4ce745bbd
Nicolas Magro <anicomagro@gmail.com>
2022-05-17 16:22:42 -0300

    commit en main

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World! 2");
 string texto = Console.ReadLine();
 Console.WriteLine("Usted escribio " + texto);
+Console.WriteLine("Rama main");
