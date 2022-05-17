1f52627d57c15bf0bb4c42053af842573090c281
Nicolas Magro <anicomagro@gmail.com>
2022-05-17 19:15:27 -0300

    terminando punto 2

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,43 +1,56 @@
-﻿Console.WriteLine("Ingrese dos numeros");
-Console.WriteLine("Ingrese el primer numero: ");
-int n1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Ingrese el segundo numero: ");
-int n2 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Que desea hacer?");
-Console.WriteLine("Presione 1 para realizar una suma");
-Console.WriteLine("Presione 2 para realizar una resta");
-Console.WriteLine("Presione 3 para realizar una multiplicacion");
-Console.WriteLine("Presione 4 para realizar una division");
-
-Console.WriteLine("Ingrese una opcion: ");
-int operacion = Convert.ToInt32(Console.ReadLine());
-while (operacion <= 0 || operacion > 4)
+﻿int calculadora = 1;
+while (calculadora == 1)
 {
-    Console.WriteLine("Ingrese una opcion correcta");
-    operacion = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Ingrese dos numeros");
+    Console.WriteLine("Ingrese el primer numero: ");
+    int n1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Ingrese el segundo numero: ");
+    int n2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Que desea hacer?");
+    Console.WriteLine("Presione 1 para realizar una suma");
+    Console.WriteLine("Presione 2 para realizar una resta");
+    Console.WriteLine("Presione 3 para realizar una multiplicacion");
+    Console.WriteLine("Presione 4 para realizar una division");
+
+    Console.WriteLine("Ingrese una opcion: ");
+    int operacion = Convert.ToInt32(Console.ReadLine());
+    while (operacion <= 0 || operacion > 4)
+    {
+        Console.WriteLine("Ingrese una opcion correcta");
+        operacion = Convert.ToInt32(Console.ReadLine());
+    }
+
+    int resultado;
+    switch (operacion)
+    {
+        case 1:
+            resultado = sumar(n1, n2);
+            Console.WriteLine("El resultado de sumar ambos numeros es: " + resultado);
+        break;
+        case 2:
+            resultado = restar(n1, n2);
+            Console.WriteLine("El resultado de restar ambos numeros es: " + resultado);
+        break;
+        case 3:
+            resultado = multiplicar(n1, n2);
+            Console.WriteLine("El resultado de multiplicar ambos numeros es: " + resultado);
+        break;
+        case 4: 
+            resultado = dividir(n1, n2);
+            Console.WriteLine("El resultado de dividir ambos numeros es: " + resultado);
+        break;
+    }
+
+    Console.WriteLine("Pulse 1 si realizara otra operacion, sino pulse cualquier numero");
+    calculadora = Convert.ToInt32(Console.ReadLine());
+
+    if (calculadora != 1)
+    {
+        Console.WriteLine("Muchas gracias por usar la calculadora :)");
+    }
 }
 
-int resultado;
-switch (operacion)
-{
-    case 1:
-        resultado = sumar(n1, n2);
-        Console.WriteLine("El resultado de sumar ambos numeros es: " + resultado);
-    break;
-    case 2:
-        resultado = restar(n1, n2);
-        Console.WriteLine("El resultado de restar ambos numeros es: " + resultado);
-    break;
-    case 3:
-        resultado = multiplicar(n1, n2);
-        Console.WriteLine("El resultado de multiplicar ambos numeros es: " + resultado);
-    break;
-    case 4: 
-        resultado = dividir(n1, n2);
-        Console.WriteLine("El resultado de dividir ambos numeros es: " + resultado);
-    break;
-}
 
 int sumar(int n1, int n2)
 {
