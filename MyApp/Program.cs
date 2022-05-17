5b3d9d13ccebb28c5b11371263e8a18181abf343
Nicolas Magro <anicomagro@gmail.com>
2022-05-17 15:29:02 -0300

    Muestra de readline en C#

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World! 2");
+string texto = Console.ReadLine();
+Console.WriteLine("Usted escribio " + texto);
