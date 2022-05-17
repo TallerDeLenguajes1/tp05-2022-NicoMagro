6dd94fe337db2fd072c35dbb906000cf7e2fb497
Nicolas Magro <anicomagro@gmail.com>
2022-05-17 16:13:47 -0300

    cambiando de rama

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,5 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World! 2");
+Console.WriteLine("Ingrese un texto: ");
 string texto = Console.ReadLine();
-Console.WriteLine("Usted escribio " + texto);
+Console.WriteLine("Ingrese un numero: ");
+string numero = Console.ReadLine();
+Console.WriteLine("Ingrese otro numero: ");
+string numero1 = Console.ReadLine();
+int resultado = Int32.Parse(numero) + Convert.ToInt32(numero1);
+Console.WriteLine("Usted escribio " + texto + numero + numero1);
+Console.WriteLine("el resultado es: " + resultado);
+Console.WriteLine("Rama 2");
